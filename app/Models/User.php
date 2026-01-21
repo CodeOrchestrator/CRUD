5677d3f61fcb5c700997d5fc5ea6328d173e8aaa
CodeOrchestrator <khudoyberganov2008@gmail.com>
2026-01-21 23:19:27 +0500

    auth tayyor

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,16 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -64,6 +66,32 @@ class User extends Authenticatable
         $this->save();
         return $this->email_verification_code;
     }
+
+    public function markEmailAsVerifiedWithCode(): void
+    {
+        $this->email_verified_at = now();
+        $this->email_verfication_code = null;
+        $this->email_verfication_code_expires_at = null;
+        $this->save();
+    }
+
+    public function isValidVerificationCode($code): bool
+    {
+        return $this->email_veerification_code == $code && $this->email_verification_code_expires_at &&
+            $this->email_verification_code_expires_at->isFuture() ;
+    }
+
+    public function getMyRoleAttribute(): ?string
+    {
+        return $this->roles()->pluck('name')->first();
+    }
+
+    public function getMyRole(): ?string
+    {
+        return $this->roles()->pluck('name')->first();
+    }
+
+
 
 
 }
